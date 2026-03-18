64959074a1217537f5ba8548f2b31882a52a1da1
Narola Harsh <51940885+narolaharsh@users.noreply.github.com>
2026-03-18 11:39:28 +0100

    Add pertrubation function

--- bilby/gw/likelihood/numerical_relative.py
+++ bilby/gw/likelihood/numerical_relative.py
@@ -1,29 +1,29 @@
 import numpy as np
 from .base import GravitationalWaveTransient
 from ...core.utils import logger
-from scipy.optimize import differential_evolution
-from ..utils import noise_weighted_inner_product
-
-
+# from scipy.optimize import differential_evolution
+# from ..utils import noise_weighted_inner_product
 
 
 class NumericalRelativeBinningGravitationalWaveTransient(GravitationalWaveTransient):
     """
     A gravitational-wave transient likelihood object.
     - This class implements a numerical relative binning scheme in contrast to the analytic one (in relative.py).
-    - This class is specifically developed to perform parameter estimation on long duration signal including earth rotation. 
+    - This class is specifically developed to perform
+    parameter estimation on long duration signal including earth rotation.
     """
 
     def __init__(
-            self, interferometers, waveform_generator, fiducial_parameters=None, 
+            self, interferometers, waveform_generator,
+            fiducial_parameters=None,
             time_marginalization=False,
             distance_marginalization=False, phase_marginalization=False, priors=None,
             distance_marginalization_lookup_table=None,
             jitter_time=True, reference_frame="sky",
             time_reference="geocenter", earth_rotation=False,
             minimum_bins=1000,
-            ):
-        
+    ):
+
         super().__init__(
             interferometers=interferometers,
             waveform_generator=waveform_generator,
@@ -37,3 +37,31 @@ class NumericalRelativeBinningGravitationalWaveTransient(GravitationalWaveTransi
             phase_marginalization=phase_marginalization,
             earth_rotation=earth_rotation,
         )
+
+    def setup_bins():
+        pass
+
+    def generate_perturbed_parameters(self):
+        perturbed_parameters = self.fiducial_parameters.copy()
+        chirp_mass_perturbation_percentage = 1e-3
+        mass_ratio_perturbation_percentage = 1e-3
+
+        chirp_mass_perturbation = 1 + (1e-2 * chirp_mass_perturbation_percentage * np.random.uniform(-1, 1, 1)[0])
+        mass_ratio_perturbation = 1 + (1e-2 * mass_ratio_perturbation_percentage * np.random.uniform(-1, 1, 1)[0])
+
+        perturbed_parameters['chirp_mass'] *= chirp_mass_perturbation
+        perturbed_parameters['mass_ratio'] *= mass_ratio_perturbation
+
+        logger.info(
+            f"Perturbed chirp mass and mass ratio: "
+            f"{perturbed_parameters['chirp_mass']} "
+            f"{perturbed_parameters['mass_ratio']}"
+        )
+        self.perturbed_parameters = perturbed_parameters
+
+    def compute_summary_data(self, frequency_bins):
+        """
+        Function to compute summary data
+        """
+        summary_data = dict
+        self.summary_data = summary_data
